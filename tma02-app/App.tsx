26356c365b8a9df5a5cfcc006e351a6f9be58011
sukisu <94705452+sukisu@users.noreply.github.com>
2025-01-24 10:41:16 +0000

    Allow the customer to view the number of votes on each report.

--- tma02-app/App.tsx
+++ tma02-app/App.tsx
@@ -211,9 +211,8 @@ const App = () => {
       <Text style={styles.text}>Reports</Text>
       {photos.map((photo: Photo, index: number) => (
           <View key={index}>
-            <PhotoEditor photo={photo} user={user} />
-            <ScaledImage uri={photo.uri} width={Dimensions.get('window').width} />
-
+            {/*<PhotoEditor photo={photo} user={user} />*/}
+            {/*<ScaledImage uri={photo.uri} width={Dimensions.get('window').width} />*/}
             <Text style={styles.text}>Comments:</Text>
             {photo.comments.length > 0 ? (
                 photo.comments.map((c, i) => (
